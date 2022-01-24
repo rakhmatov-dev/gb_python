468cea982006e7545ddb4b83bd3132f27bfeeb3d
rakhmatov93khondamir <khondamir.rakhmatov@gmail.com>
2022-01-24 18:54:32 +0500

    lesson-1

--- main.py
+++ main.py
@@ -1,16 +1,84 @@
-# This is a sample Python script.
 
-# Press Shift+F10 to execute it or replace it with your code.
-# Press Double Shift to search everywhere for classes, files, tool windows, actions, and settings.
+# Урок 1
+
+# Задание 1
+a = 12
+b = 21
+print(f"a={a}, b={b}")
+a, b = b, a
+print(f"a={a}, b={b}")
+
+name = input("What's your name? ")
+age = input("How old are you? ")
+print(f"Your name is {name} and you are {age} years old.")
+#
+
+# Задание 2
+seconds = int(input("Введите время в секундах: "))
+
+minutes = seconds // 60
+seconds = seconds % 60
+
+hours = minutes // 60
+minutes = minutes % 60
+
+print(f"{hours:02}:{minutes:02}:{seconds:02}")
+#
+
+# Задание 3
+n = int(input("Введите число: "))
+nn = int(str(n) + str(n))
+nnn = int(str(n) + str(n) + str(n))
+print(f"Сумма: {n + nn + nnn}")
+#
+
+# Задание 4
+n = int(input("Введите число: "))
+max_digit = -1
+while n % 10 != 0:
+    if n % 10 > max_digit:
+        max_digit = n % 10
+    n = n//10
+
+if n > max_digit:
+    max_digit = n
+
+print(f"Самая большая цифра в числе: {max_digit}")
+#
+
+# Задание 5
+proceeds = int(input("Введите выручку: "))
+costs = int(input("Введите издержки: "))
+
+if proceeds > costs:
+    print(f"Прибыль: {proceeds - costs}")
+else:
+    print(f"Убыток: {proceeds - costs}")
+#
+
+# Задание 6
+proceeds = int(input("Введите выручку: "))
+costs = int(input("Введите издержки: "))
+
+if proceeds > costs:
+    print(f"Прибыль: {proceeds - costs}")
+    print(f"Рентабельность выручки: {(proceeds - costs)/proceeds}")
+    employee_amount = int(input("Введите количество сотрудников фирмы: "))
+    print(f"Прибыль фирмы в расчёте на одного сотрудника: {(proceeds - costs)/employee_amount}")
+else:
+    print(f"Убыток: {proceeds - costs}")
+#
+
+# Задание 7
+a = int(input("Введите a: "))
+b = int(input("Введите b: "))
+n = 1
+while a < b:
+    n += 1
+    a *= 1.1
+
+print(n)
+#
 
 
-def print_hi(name):
-    # Use a breakpoint in the code line below to debug your script.
-    print(f'Hi, {name}')  # Press Ctrl+F8 to toggle the breakpoint.
 
-
-# Press the green button in the gutter to run the script.
-if __name__ == '__main__':
-    print_hi('PyCharm')
-
-# See PyCharm help at https://www.jetbrains.com/help/pycharm/
